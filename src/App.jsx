2945636ea7f684eda6719da29a607d651c626e3f
pedro-henrique-oliveira <henriqueoliveiradasilvapedro1@gmail.com>
2025-11-25 10:46:33 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -85,7 +85,7 @@ function App() {
 
       <section id="s6" className={styles.s6}>
         <h2 className={styles.teste}></h2>
-        <iframe width="1800" height="800" src="https://www.youtube.com/embed/m-cazbbExmY?si=tf_KK1WSvrQZvzyV" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        <iframe width="1850" height="800" src="https://www.youtube.com/embed/m-cazbbExmY?si=tf_KK1WSvrQZvzyV" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
         
       </section>
     </main>
